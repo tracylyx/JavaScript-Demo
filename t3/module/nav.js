a0546acf1069d45dc3d73995ef7b7325560f6c47
tracylyx <tracylyx1992@gmail.com>
2015-04-22 01:24:24 +0800

    add comments

--- t3/module/nav.js
+++ t3/module/nav.js
@@ -1,10 +1,10 @@
-Box.Application.addModule('nav', function(context) {
+Box.Application.addModule('nav', function(context) {	// context模块的触碰点
 	'use strict';
 
 	//-------------------------
 	// Private
 	// ------------------------
-	var i,
+	var i,		// 超过一个地方使用，就在这里定义，这样可以在多出使用
 		moduleE1,
 		listE1,
 		boxE1;
@@ -15,8 +15,10 @@ Box.Application.addModule('nav', function(context) {
 	// ------------------------
 	
 	return {
+		// init(),destroy()这两个方法和生命周期有关
+		// 虽然不是必须使用，但是建议模块进行初始化和销毁
 		init: function() {
-			moduleE1 = context.getElement();
+			moduleE1 = context.getElement();	// 取回代表模块的元素
 			listE1 = moduleE1.querySelectorAll('li');
 			boxE1 = moduleE1.querySelectorAll('.table-cont');
 		},
